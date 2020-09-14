a4eecbf066f82496cab72c870ee9e8ae4447c418
Omar Zapata <Omar.Zapata@cern.ch>
2020-09-14 10:45:47 +0200

    added all launcher in the same package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,38 +1,12 @@
-import { VDomRenderer } from '@jupyterlab/apputils';
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { ILauncher } from '@jupyterlab/launcher';
 import { LabIcon } from '@jupyterlab/ui-components';
-import * as React from 'react';
-import { ILauncher, LauncherModel } from '@jupyterlab/launcher';
 export declare const swanProjectIcon: LabIcon;
-export declare const swanReadmeIcon: LabIcon;
-export declare const swanConfigIcon: LabIcon;
 /**
- * A virtual-DOM-based widget for the Launcher.
+ * A service providing an interface to the the launcher.
  */
-export declare class SWANLauncher extends VDomRenderer<LauncherModel> {
-    /**
-     * Construct a new launcher widget.
-     */
-    constructor(options: ILauncher.IOptions);
-    /**
-     * The cwd of the launcher.
-     */
-    get cwd(): string;
-    set cwd(value: string);
-    /**
-     * Whether there is a pending item being launched.
-     */
-    get pending(): boolean;
-    set pending(value: boolean);
-    /**
-     * Launch dialog to change the stack of the project.
-     */
-    protected changeStack(): void;
-    /**
-     * Render the launcher to virtual DOM nodes.
-     */
-    protected render(): React.ReactElement<any> | null;
-    private _commands;
-    private _callback;
-    private _pending;
-    private _cwd;
-}
+declare const ProjectLauncher: JupyterFrontEndPlugin<ILauncher>;
+/**
+ * Export the plugin as default.
+ */
+export default ProjectLauncher;
